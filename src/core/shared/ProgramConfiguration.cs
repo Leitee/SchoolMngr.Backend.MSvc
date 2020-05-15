a40e49a07a6494c8bda166fc91956fecf5687cf2
Leonardo <leitee23@hotmail.com>
2020-05-14 21:26:13 -0300

    updating folder structure

---
+++ src/core/shared/ProgramConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Configuration;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace SchoolMngr.Core.Shared
+{
+    public static class ProgramConfiguration
+    {
+        private static readonly string environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+        public static ILogger CreateSerilogLogger(IConfiguration configuration, string appName)
+        {
+            //var seqServerUrl = configuration["Serilog:SeqServerUrl"];
+            //var logstashUrl = configuration["Serilog:LogstashgUrl"];
+            return new LoggerConfiguration()
+                .Enrich.WithProperty("ApplicationName", appName)
+                .Enrich.WithProperty("Environment", environmentName)
+                .Enrich.FromLogContext()
+                //.WriteTo.Seq(string.IsNullOrWhiteSpace(seqServerUrl) ? "http://seq" : seqServerUrl)
+                //.WriteTo.Http(string.IsNullOrWhiteSpace(logstashUrl) ? "http://logstash:8080" : logstashUrl)
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+        }
+
+        public static IConfiguration GetConfiguration()
+        {
+            return new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{environmentName}.json", optional: true)
+                .AddEnvironmentVariables()
+                .Build();
+        }
+    }
+}
